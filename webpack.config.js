e58fbd2825259d30f519dc38b875768a55c78bf4
Stuart <stuart.carlisle@googlemail.com>
2019-09-04 15:41:43 +0100

    10th comm

--- webpack.config.js
+++ webpack.config.js
@@ -48,14 +48,18 @@ module.exports= (env) => {
     plugins:[
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY':JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN':JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL':JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID':JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.ALGOLIA_APP_ID':JSON.stringify(process.env.ALGOLIA_APP_ID),
-        'process.env.ALGOLIA_API_KEY':JSON.stringify(process.env.ALGOLIA_API_KEY)      
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_API_KEY2': JSON.stringify(process.env.FIREBASE_API_KEY2),
+        'process.env.FIREBASE_DATABASE_URL2': JSON.stringify(process.env.FIREBASE_DATABASE_URL2),
+        'process.env.FIREBASE_AUTH_DOMAIN2': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN2),
+        'process.env.FIREBASE_STORAGE_BUCKET2': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET2),
+        'process.env.ALGOLIA_APP_ID': JSON.stringify(process.env.ALGOLIA_APP_ID),
+        'process.env.ALGOLIA_API_KEY': JSON.stringify(process.env.ALGOLIA_API_KEY)      
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
