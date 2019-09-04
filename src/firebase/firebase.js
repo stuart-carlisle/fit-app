4ca47bfec576b289c7369c72870b3cd134f7fa45
Stuart <stuart.carlisle@googlemail.com>
2019-09-04 14:17:35 +0100

    8th comm

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,7 @@ import * as firebase from 'firebase'
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL:process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
@@ -12,10 +12,10 @@ const primary = firebase.initializeApp(config);
 const database = primary.database();
 
 const secondaryAppConfig = {
-  apiKey: process.env.FIREBASE_API_KEY2,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN2,
-  databaseURL: "https://fitapp-public.firebaseio.com",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET2
+  //apiKey: process.env.FIREBASE_API_KEY2,
+  //authDomain: process.env.FIREBASE_AUTH_DOMAIN2,
+  databaseURL: "https://fitapp-public.firebaseio.com"//,
+  //storageBucket: process.env.FIREBASE_STORAGE_BUCKET2
 }
 const secondary = firebase.initializeApp(secondaryAppConfig, "secondary")
 const databasePublic = secondary.database()
