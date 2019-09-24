2e0911cefb563438c3ec047b9cade2490571779a
Stuart <stuart.carlisle@googlemail.com>
2019-09-24 10:51:58 +0100

    rounded serving amounts in the diary

--- src/components/DiaryFoodsListItem.js
+++ src/components/DiaryFoodsListItem.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export const DiaryFoodsListItem = ({ description, totalEnergy, id, serving, totalServingSize }) => {
   const servingUnit = !!serving.servingSize.unit ? serving.servingSize.unit :'g'
-  //const totalServingSizeRounded = Math.round(totalServingSize)
+  const totalServingSizeRounded = Math.round(totalServingSize)
   const totalEnergyRounded = Math.round(totalEnergy)
   return(
     <Link className="list-item" to={`/update-food/${id}`}>
       <div>
-        <p className="list-item__title">{description} - {totalServingSize}{servingUnit}</p>
+        <p className="list-item__title">{description} - {totalServingSizeRounded}{servingUnit}</p>
       </div>
       <div>
         <p className="list-item__data">{totalEnergyRounded}</p>
